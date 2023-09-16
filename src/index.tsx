7bee383482f4cbaabe5cf14321e7da15ed622ac1
Nikita Vinogradov <n.a.vinogradov94@gmail.com>
2023-09-16 17:37:46 +0300

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,15 @@ import App from './components/app/app';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </Provider>
 );
